27077f2d7c33818863a52eaaa6f701b9951bfffb
abbf2501 <abbf2501@gmail.com>
2016-03-03 12:06:03 -0500

    test avecJunit
    premier test sur les données

--- app/src/androidTest/java/com/example/lamas/testdataxml/ApplicationTest.java
+++ app/src/androidTest/java/com/example/lamas/testdataxml/ApplicationTest.java
@@ -1,13 +1,75 @@
 package com.example.lamas.testdataxml;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+    private Data data;
+    private Monument obj;
+    private Context context;
+    private Monument monum;
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+        context = mActivityRule.getActivity().getApplicationContext();
+        data = Data.getInstance(context);
+        assertNotNull(data);
+        monum = new Monument(2, 45.3795022, -71.931661, "Faculté des lettres et sciences humaines", "La Faculté des lettres et sciences humaines offre 14 programmes de formations initiales, dont quatre suivant le régime coopératif, pouvant mener à des carrières en politique, en géomatique, en musique, en psychologie, en communication et en marketing, en littérature, en travail social, en traduction, en histoire, et bien d’autres.", "super naze leur site", null, new ArrayList<String>() {{
+            add("lundi 08:00-12:00 13:30-16:00");
+            add("mardi 08:00-12:00 13:30-16:00");
+            add("mercredi 08:00-12:00 13:30-16:00");
+            add("jeudi 08:00-12:00 13:30-16:00");
+            add("vendredi 08:00-12:00 13:30-16:00");
+        }});
+    }
+
+    @Test
+    public void nombreMonuments() {
+        assertEquals(data.getMonuments().size(), 10);
+    }
+
+    @Test
+    public void nombreParcours() {
+        assertEquals(data.getParcourses().size(), 4);
+    }
+
+    @Test
+    public void monumentExiste() {
+        assertNotNull(monum);
+        assertFalse(data.getMonuments().containsValue(monum));
+    }
+
+    @Test
+    public void monumentExisteDansParcours() {
+        //  assertNotNull(monum);
+        //  assertFalse(data.getParcourses().get(1).getMonuments().equals(monum));
+        //  assertFalse(data.getParcourses().get(2).getMonuments().equals(monum));
+        //  assertFalse(data.getParcourses().get(3).getMonuments().equals(monum));
+        assertFalse(data.getParcourses().get(4).getMonuments().equals(monum));
     }
 }
