6939320e4d74db236be032ee797a537e9548525a
lang2511 <genevieve-06@hotmail.com>
2016-03-09 14:46:14 -0500

    last commit "InformationActivity"
    personnalisation de l'activité information

--- app/src/main/java/com/example/lamas/testdataxml/InformationActivity.java
+++ app/src/main/java/com/example/lamas/testdataxml/InformationActivity.java
@@ -2,6 +2,9 @@ package com.example.lamas.testdataxml;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
@@ -9,8 +12,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
-public class InformationActivity extends Activity {
+public class InformationActivity extends Activity implements
+        TextToSpeech.OnInitListener {
+
+    private TextToSpeech textToSpeech;
+    private Data data;
+
     LieuxAdapter lieuxAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader = new ArrayList<>();
@@ -20,6 +29,8 @@ public class InformationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        textToSpeech = new TextToSpeech(this, this);
 
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
@@ -42,10 +53,76 @@ public class InformationActivity extends Activity {
         TextView txtListChild = (TextView) findViewById(R.id.nom_lieu);
         txtListChild.setText(monument.getName());
 
+        //Personnalisation de l'application
+        data = Data.getInstance(getApplicationContext());
+        txtListChild.setTextColor(data.getParameters().getCouleurTexte());
+        txtListChild.setBackgroundColor(data.getParameters().getCouleurBackground());
+        txtListChild.setTypeface(data.getParameters().getTypeface());
+
         lieuxAdapter = new LieuxAdapter(getApplicationContext(), listDataHeader, listDataChild);
 
         expListView.setAdapter(lieuxAdapter);
 
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                String toSpeak = listDataHeader.get(groupPosition).toString();
+                convertTextToSpeech(toSpeak);
+            }
+        });
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                String toSpeak = listDataChild.get(listDataHeader.get(groupPosition)).get(
+                        childPosition).toString();
+                convertTextToSpeech(toSpeak);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.CANADA_FRENCH);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("error", "This Language is not supported");
+            } else {
+                convertTextToSpeech("Parcours");
+            }
+        } else {
+            Log.e("error", "Initilization Failed!");
+        }
+    }
+
+    /**
+     * Releases the resources used by the TextToSpeech engine. It is good
+     * practice for instance to call this method in the onDestroy() method of an
+     * Activity so the TextToSpeech engine can be cleanly stopped.
+     *
+     * @see android.app.Activity#onDestroy()
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        textToSpeech.shutdown();
+    }
+
+    /**
+     * Speaks the string using the specified queuing strategy and speech
+     * parameters.
+     */
+    private void convertTextToSpeech(String text) {
+        //String text = inputText.getText().toString();
+        //if (null == text || "".equals(text)) {
+        //    text = "Please give some input.";
+        //}
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
 }
